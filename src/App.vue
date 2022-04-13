a7e8283e14b8678d4140c2ed18d7bb9682219897
Bryan Valencia <bryan.val93@gmail.com>
2022-04-12 20:54:12 -0400

    figured out how to add NavBar component and naming conventions for components as well as where to export, import and call components. Time for a run git push

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Vue App: Garza Rest...</title>
+    </head>
+    <body>
+      <NavBar></NavBar>
+    </body>
+  </html>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+import NavBar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="navbar">
+        Hello I'm a navbar!
+    </div>
+</template>
+<script>
+    export default {
+    name: 'NavBar',
+    }
+</script>
