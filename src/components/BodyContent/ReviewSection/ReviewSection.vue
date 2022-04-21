0d705a84390629cffcb8370a10e157c5e1fac4aa
Bryan Valencia <bryan.val93@gmail.com>
2022-04-21 12:53:57 -0400

    added basic layout in comments for review section during lunch

---
+++ src/components/BodyContent/ReviewSection/ReviewSection.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="reviewContentContainer">
+        <!-- Picture of Work -->
+        <!-- Review Paragraph -->
+        <!-- Review Customer Section -->
+            <!-- Customer Name -->
+            <!-- Review Date -->
+            <!-- Review Stars -->
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
